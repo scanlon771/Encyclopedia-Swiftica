71c56c1681595f086d250999e1ea2b6e18f9b334
Kenneth Scanlon <scanlon771@gmail.com>
2024-04-14 18:55:39 -0500

    Project created.

---
+++ Encyclopedia Swiftica/Encyclopedia_SwifticaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Encyclopedia_SwifticaApp.swift
+//  Encyclopedia Swiftica
+//
+//  Created by Kenneth Scanlon on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Encyclopedia_SwifticaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
